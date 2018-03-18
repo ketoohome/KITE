003ca29e458d87f887e212e706772b3472f650da
ketoohome <35617875+ketoohome@users.noreply.github.com>
2018-03-18 15:24:28 +0800

    Control
    1. 增加美术目录，添加参考图集
    2. 增加更换拾取风筝，挂接功能
    3. 增加更换绳子的功能，绳子包含熟韧性，重量，最大承受力等属性

--- Project/Assets/Test/Kite/Character/Character.cs
+++ Project/Assets/Test/Kite/Character/Character.cs
@@ -8,10 +8,9 @@ public class Character : MonoBehaviour {
 	Animator mHand = null;
 	Transform mBobbinWinder = null;
 
-	float m_TargetRopeLength = 5;
+	float m_TargetRopeLength = 1;
 	// 
 	void Start(){
-		mRope = transform.Find ("Rope").GetComponent<Rope>();
 		mHand = transform.Find ("Hand").GetComponent<Animator>();
 		mBobbinWinder = transform.Find ("Hand/BobbinWinder/Node");
 	}
@@ -38,11 +37,65 @@ public class Character : MonoBehaviour {
 			mHand.SetInteger ("State",2);
 		}
 
-		m_TargetRopeLength = Mathf.Clamp (m_TargetRopeLength,1,200);
-		float curr = Mathf.Lerp (mRope.RopeLength,m_TargetRopeLength,Time.deltaTime * 10);
+		if (Input.GetMouseButtonDown (0)) {
+			PickUpKite ();
+		}
 
-		mRope.RopeLength = curr;
-		mBobbinWinder.localRotation = Quaternion.Euler(new Vector3(0,0,-curr *180));
+		if (Input.GetMouseButtonDown (1)) {
+			FixRope ();
+		}
 
+		if (mRope != null) {
+			m_TargetRopeLength = Mathf.Clamp (m_TargetRopeLength,0,200);
+			float curr = Mathf.Lerp (mRope.RopeLength,m_TargetRopeLength,Time.deltaTime * 10);
+
+			mRope.RopeLength = curr;
+			mBobbinWinder.localRotation = Quaternion.Euler(new Vector3(0,0,-curr *180));
+		}
+
+		if (Input.GetKeyDown (KeyCode.Alpha1)) {
+			if(mRope == null) CreateRope("Rope_01");
+		}
+		if (Input.GetKeyDown (KeyCode.Alpha2)) {
+			if(mRope == null) CreateRope("Rope_02");
+		}
+		if (Input.GetKeyDown (KeyCode.Alpha3)) {
+			if(mRope == null) CreateRope("Rope_03");
+		}
+	}
+
+	/// <summary>
+	/// 拾取风筝
+	/// </summary>
+	void PickUpKite(){
+		if (mRope == null) return;
+		Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+		RaycastHit hit;
+		if (Physics.Raycast (ray, out hit, 3)) {
+			Kite kite = hit.collider.transform.GetComponent<Kite> ();
+			if (kite != null) mRope.ConnectKite (hit.collider.gameObject);
+		}
+	}
+
+	/// <summary>
+	/// 固定风筝线
+	/// </summary>
+	void FixRope(){
+		if (mRope == null) return;
+		Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+		RaycastHit hit;
+		if (Physics.Raycast (ray, out hit, 3)) {
+			mRope.DisconnectKite (hit.point,hit.collider.gameObject);
+			mRope = null;
+		}
+	}
+
+	/// <summary>
+	/// 更换绳子
+	/// </summary>
+	void CreateRope(string name){
+		m_TargetRopeLength = 0.5f;
+		GameObject obj = Instantiate<GameObject> (Resources.Load<GameObject> (name),transform.position,transform.rotation,transform);
+		mRope = obj.GetComponent<Rope> ();
 	}
 }
---
+++ Project/Assets/Test/Kite/Kite/Kite.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Kite.
+/// </summary>
+public class Kite : MonoBehaviour {
+
+	Rigidbody mRig;
+	// Use this for initialization
+	void Start () {
+		mRig =  GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Wind.Instance.WindForce (mRig);
+		// 风筝的朝向只跟风向有关系
+		//Debug.Log("速度：" + rig.velocity.magnitude);
+	}
+}
